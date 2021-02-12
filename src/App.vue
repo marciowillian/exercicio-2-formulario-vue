5e7693dd5f38be7c3cb1a73f7604c49d6959c10e
Marcio Cardoso <marcio.cardoso@armateus.com.br>
2021-02-12 09:12:04 -0300

    Innitial commit

---
+++ src/App.vue
@@ -0,0 +1,133 @@
+<template>
+  <div id="app">
+    <h1>Formulário Desafio</h1>
+    <div class="conteudo">
+      <form class="painel" v-if="!enviado">
+        <div class="cabecalho">Formulário</div>
+        <NomeCompleto  v-model="nomeCompleto"/>
+        <Rotulo nome="E-mail">
+          <input type="text" v-model.lazy.trim="usuario.email" />
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <input type="password" v-model="usuario.senha" />
+        </Rotulo>
+        <Rotulo nome="Armazenar dados">
+          <Escolha v-model="escolha" />
+        </Rotulo>
+
+        <button @click.prevent="enviar">Enviar</button>
+        <!-- Exercicio 03 -->
+        <!-- Crie um componente personalizado NomeCompleto -->
+        <!-- Esse componente deve receber Nome e Sobrenome -->
+      </form>
+      <div class="painel" v-else>
+        <div class="cabecalho">Resultado</div>
+
+        <Rotulo nome="Nome">
+          <span>{{ nomeCompleto.nome }}</span>
+        </Rotulo>
+        <Rotulo nome="Sobrenome">
+          <span>{{ nomeCompleto.sobrenome }}</span>
+        </Rotulo>
+        <Rotulo nome="E-mail">
+          <span>{{ usuario.email }}</span>
+        </Rotulo>
+        <Rotulo nome="Senha">
+          <span>{{ usuario.senha }}</span>
+        </Rotulo>
+        <Rotulo nome="Armazenar dados">
+          <span>{{ escolha }}</span>
+        </Rotulo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./components/Rotulo.vue";
+import Escolha from "./components/Escolha.vue";
+import NomeCompleto from "./components/NomeCompleto.vue";
+
+export default {
+  name: "app",
+  components: { Rotulo, Escolha, NomeCompleto },
+  data() {
+    return {
+      escolha: false,
+      enviado: false,
+      nomeCompleto: {
+        nome: "",
+        sobrenome: "",
+      },
+      usuario: {
+        email: "",
+        senha: "",
+        armazenarDados: false,
+      },
+    };
+  },
+  methods: {
+    enviar() {
+      this.enviado = !this.enviado;
+    },
+  },
+};
+</script>
+
+<style>
+body {
+  background-color: #ececec;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.conteudo {
+  display: flex;
+}
+
+.painel {
+  flex: 1;
+  background: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.painel .cabecalho {
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+}
+
+#app form button {
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+
+#app h1 {
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
+}
+
+.mr-4 {
+  margin-right: 40px;
+}
+</style>
---
+++ src/components/NomeCompleto.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Rotulo nome="Nome">
+      <input type="text" @input="alterarNome" :name="name.nome" />
+    </Rotulo>
+    <Rotulo nome="Sobrenome">
+      <input type="text" @input="alterarSobrenome" :name="name.sobrenome" />
+    </Rotulo>
+  </div>
+</template>
+
+<script>
+import Rotulo from "./Rotulo";
+export default {
+  props: {
+    name: { type: Object, required: true },
+  },
+  components: { Rotulo },
+  methods: {
+    alterarNome(event) {
+      this.$emit("input", {
+        nome: event.target.value,
+        sobrenome: this.value.sobrenome,
+      });
+    },
+    alterarSobrenome(event) {
+      this.$emit("input", {
+        nome: this.value.nome,
+        sobrenome: event.target.value,
+      });
+    },
+  },
+};
+</script>
